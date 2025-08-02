9168cc12611b803b4d8fa2b1e3882e52b85627d0
Smiliya <smiliyasmiliya9944@gmail.com>
2025-08-02 23:38:00 +0530

    Update Cube.java

--- Cube.java
+++ Cube.java
@@ -1,19 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cube;
 
-/**
- *
- * @author 1BSCCSB23
- */
 public class Cube {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int side=5;
         float volume=side*side*side;
