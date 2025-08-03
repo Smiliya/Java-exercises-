fc4831bf48a1c0074ad675ff90f0ec0b8257d33a
Smiliya <smiliyasmiliya9944@gmail.com>
2025-08-03 14:21:48 +0530

    Create Simple Interest.java

---
+++ Simple Interest.java
@@ -0,0 +1,11 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        double principal = 1000, rate = 5, time = 2;
+
+        double si = (principal * rate * time) / 100;
+
+        System.out.println("Simple Interest = " + si);
+    }
+}
+output:
+  Simple Interest = 100.0
