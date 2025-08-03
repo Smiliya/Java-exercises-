bf98f8c4f9e164d9cc614e28275e8e0e2c756c8c
Smiliya <smiliyasmiliya9944@gmail.com>
2025-08-03 14:16:59 +0530

    Create Count digit .java

---
+++ Count digit .java
@@ -0,0 +1,14 @@
+public class CountDigits {
+    public static void main(String[] args) {
+        int num = 12345, count = 0;
+
+        while (num != 0) {
+            num /= 10;
+            count++;
+        }
+
+        System.out.println("Total digits: " + count);
+    }
+}
+output:
+    Total digits: 5
