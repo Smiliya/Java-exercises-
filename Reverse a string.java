c117b13d4b348ce5d7301445a39e14da6a637029
Smiliya <smiliyasmiliya9944@gmail.com>
2025-08-03 14:27:14 +0530

    Create Reverse a string.java

---
+++ Reverse a string.java
@@ -0,0 +1,16 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+}
+output:
+  Original: Hello, World!
+Reversed: !dlroW ,olleH
