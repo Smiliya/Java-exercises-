219e281f4b9568dcf1adff09cb21a0c5921ba244
Smiliya <smiliyasmiliya9944@gmail.com>
2025-08-03 14:03:13 +0530

    Create Square and cube of number.java

---
+++ Square and cube of number.java
@@ -0,0 +1,10 @@
+public class SquareCube {
+    public static void main(String[] args) {
+        int num = 3;
+        System.out.println("Square: " + (num * num));
+        System.out.println("Cube: " + (num * num * num));
+    }
+}
+output:
+    Square: 9  
+Cube: 27
