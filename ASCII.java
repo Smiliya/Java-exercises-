8999d6e6b2c505bc91f975aa8c6ffa0c46250a8f
Smiliya <smiliyasmiliya9944@gmail.com>
2025-08-03 13:57:42 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,10 @@
+public class AsciiValue {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int ascii = ch;
+        System.out.println("ASCII value of " + ch + " is " + ascii);
+    }
+}
+
+output:
+  ASCII value of A is 65
