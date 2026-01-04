b8675a70d953af5263ff5dbf5f8bcbb4218ccea9
Smiliya sathiyadass S <smiliyasmiliya9944@gmail.com>
2026-01-04 21:38:58 +0530

    Delete Area of circle.java

--- Area of circle.java
+++
@@ -1,9 +0,0 @@
-public class AreaOfCircle {
-    public static void main(String[] args) {
-        double radius = 7.0;
-        double area = Math.PI * radius * radius;
-        System.out.println("Area = " + area);
-    }
-}
-output:
-   Area = 153.94
